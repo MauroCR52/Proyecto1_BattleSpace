d2169a0342402aa010ce253945c3e103804c398d
MauroCR52 <mauluna52@gmail.com>
2023-04-03 23:02:32 -0600

    Creacion de botones y ventana de los niveles

---
+++ Bullet.h
@@ -0,0 +1,35 @@
+#ifndef PROYECTO1_BATTLESPACE_BULLET_H
+#define PROYECTO1_BATTLESPACE_BULLET_H
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System/Clock.hpp>
+#include "iostream"
+
+using namespace sf;
+
+class Bullet {
+private:
+
+    Sprite shape;
+
+    Vector2f direction;
+    float movementSpeed;
+
+public:
+    Bullet();
+    Bullet(Texture * texture, float pos_x, float pos_y, float dir_x, float dir_y, float movement_speed);
+    virtual ~Bullet();
+
+    const FloatRect getBounds() const;
+
+    void update();
+    void render(RenderTarget* target);
+
+};
+
+
+#endif //PROYECTO1_BATTLESPACE_BULLET_H
