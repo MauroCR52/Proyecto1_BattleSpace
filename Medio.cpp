97b3a2b59f17758bc7893e7080837c44e184db7f
Andrea Fernández <andreafernez@estudiantec.cr>
2023-04-14 15:35:20 -0600

    Implementacion de niveles Medio y Dificil, falta cambiar velocidades de nave, balas y enemigos

--- Medio.cpp
+++ Medio.cpp
@@ -1,5 +1,9 @@
 #include "iostream"
 #include "Medio.h"
+#include "Bullet.h"
+#include <chrono>
+#include <thread>
+
 void Medio::initWindow() {
     this->window = new RenderWindow(VideoMode(800, 600), "Medio - Battlespace", Style::Titlebar | Style::Close);
     this->window->setFramerateLimit(144);
@@ -9,29 +13,85 @@ void Medio::initWindow() {
 Medio::Medio() {
     this->initWindow();
     this->initPlayer();
+    this->initTextures();
+    this->initBulletGUI();
+    this->initBulletCollectorGUI();
+    this->initPointGUI();
+    this->initOleadasGUI();
+    this->initCantEnemies();
+    this->initBackground();
+    this->initSystems();
+    this->initDelay();
+    this->initEnemies();
+    this->initEnemiesR();
+
 }
 
 Medio::~Medio() {
     delete this->window;
     delete this->player;
+
+    for (auto &i : this->textures){
+        delete i.second;
+    }
+
+    for (auto *i : this->bullets){
+        delete i;
+    }
+
+    for (auto *i : this->enemies){
+        delete i;
+    }
+    for (auto *i : this->enemiesR){
+        delete i;
+    }
 }
 
 void Medio::run() {
     while (this->window->isOpen()){
-        this->update();
+        if (this->oleadas <= 5){
+            this->update();
+        }
         this->render();
     }
 }
 
 void Medio::update() {
+
+    //this->updateArduino();
+
     this->updatePollEvents();
     this->updateInput();
+    this->updateDelay();
+    this->player->update();
+    this->updateCollision();
+    this->updateBullets();
+    this->updateEnemiesAndCombat();
+    this->updateEnemiesRAndCombat();
+    this->updateGUI();
+    this->updateBackground();
 }
 
 void Medio::render() {
     this->window->clear(Color(137, 155, 176, 255));
-    //Enseña la ventana
+
+    this->renderBackground();
+
     this->player->render(*this->window);
+
+    for (auto *bullet : this->bullets){
+        bullet->render(this->window);
+
+    }
+    for (auto *enemy : this->enemies){
+        enemy->render(*this->window);
+    }
+    for (auto *enemyR : this->enemiesR){
+        enemyR->render(*this->window);
+    }
+
+    this->renderGUI();
+
     this->window->display();
 
 }
@@ -39,6 +99,37 @@ void Medio::render() {
 
 void Medio::initPlayer() {
     this->player = new Player();
+}
+
+void Medio::initTextures() {
+
+    this->textures["BULLET"] = new Texture();
+    this->textures["BULLET"]->loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/Textures/firebullet.png");
+
+}
+
+void Medio::updateBullets() {
+    unsigned  counter = 0;
+    for (auto *bullet : this->bullets){
+        bullet->update();
+
+        //Bullet culling right screen
+        if (bullet->getBounds().left + bullet->getBounds().width > 800.f)
+        {
+            if (this->balas != 0){
+                // Modificar para en vez de borrar, se vaya al collector
+                delete this->bullets.at(counter);
+                this->bullets.erase(this->bullets.begin() + counter);
+                --counter;
+                this->collector += 1;
+            } else {
+                ::delete this->bullets.at(counter);
+                this->bullets.erase(this->bullets.begin() + counter);
+                --counter;
+            }
+        }
+        ++counter;
+    }
 }
 
 void Medio::updatePollEvents() {
@@ -49,11 +140,345 @@ void Medio::updatePollEvents() {
         if (e.Event::key.code == Keyboard::Escape)
             this->window->close();
     }
+    if (this->balas == 0){
+        this->player->setDamage(5);
+    }
 }
 
 void Medio::updateInput() {
+    if (this->oleadas == 1){
+        char soundAction = '1';
+        //boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+    }
+
+    // Mover el jugador
     if(Keyboard::isKeyPressed(Keyboard::Up))
         this->player->move(-1.f);
     if(Keyboard::isKeyPressed(Keyboard::Down))
         this->player->move(1.f);
+
+    if (!this->delay){
+        if (this->balas != 0){
+            if (this->window->isOpen() && this->player->canAttack()){
+                this->bullets.push_back(::new Bullet(this->textures["BULLET"], this->player->getPost().x, this->player->getPost().y + 18.f, 1.f, 0.f,5.f));
+                this->balas--;
+            }
+        }
+        if (this->balas == 0 && this->collector != 0){
+            if (this->window->isOpen() && this->player->canAttack()){
+                this->bullets.push_back(new Bullet(this->textures["BULLET"], this->player->getPost().x, this->player->getPost().y + 18.f, 1.f, 0.f,5.f));
+                this->collector--;
+            }
+        }
+    }
 }
+
+void Medio::initDelay() {
+    this->delayTimerMax = 200.f;
+    this->delayTimer = 0.f;
+}
+
+void Medio::initEnemies() {
+    this->spawnTimerMax = 180.f;
+    this->spawnTimer = this->spawnTimerMax;
+}
+
+void Medio::initEnemiesR() {
+    this->spawnTimerMaxR = 500.f;
+    this->spawnTimerR = 400.f;
+
+}
+
+void Medio::updateEnemiesRAndCombat() {
+    //Aparicion de enemigos
+    if (this->canSpawn){
+        if (this->cant_enemigos != 0){
+            this->spawnTimerR += 0.5f;
+            if (this->spawnTimerR >= this->spawnTimerMaxR){
+                this->enemiesR.push_back(new EnemyR(800.f, rand() % this->window->getSize().y-60.f));
+                this->cant_enemigos--;
+                this->spawnTimerR = 0.f;
+            }
+        }
+    }
+    //Borrar enemigos
+    for (int i = 0; i < this->enemiesR.size(); ++i) {
+        bool enemy_removed = false;
+        this->enemiesR[i]->update();
+
+        for (size_t k = 0; k < this->bullets.size() && !enemy_removed; k++) {
+            if (this->bullets[k]->getBounds().intersects(this->enemiesR[i]->getBounds())) {
+                this->enemiesR[i]->setHp(this->enemiesR[i]->gethp()-this->player->getDamage());
+                if (this->enemiesR[i]->gethp() <= 0){
+                    this->bullets.erase(this->bullets.cbegin() + k);
+                    this->enemiesR.erase(this->enemiesR.cbegin() + i);
+                    this->puntos++;
+                    enemy_removed = true;
+                }
+                else {
+                    this->bullets.erase(this->bullets.cbegin() + k);
+                }
+            }
+        }
+        if(!enemy_removed){
+            // Remover nave cuando llega al otro lado de la ventana (Implementar buzzer de arduino)
+            if (this->enemiesR[i]->getBounds().left < 0.f)
+            {
+                //Enviar mensaje a Arduino
+                this->enemiesR.erase(this->enemiesR.cbegin() + i);
+                char soundAction = 'S';
+                //boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+
+            }
+            if (this->enemiesR[i]->getBounds().top < 0.f && this->enemiesR[i]->getMoveY() < 0){
+                this->enemiesR[i]->setMoveY(1.5f);
+            }
+            if (this->enemiesR[i]->getBounds().top > 540.f && this->enemiesR[i]->getMoveY() > 0){
+                this->enemiesR[i]->setMoveY(-1.5f);
+            }
+        }
+    }
+}
+void Medio::updateEnemiesAndCombat() {
+    //Aparicion de enemigos
+    if (this->canSpawn){
+        if (this->cant_enemigos != 0){
+            this->spawnTimer += 0.5f;
+            if (this->spawnTimer >= this->spawnTimerMax){
+                this->enemies.push_back(new Enemy(800.f, rand() % 550));
+                this->cant_enemigos--;
+                this->spawnTimer = 0.f;
+            }
+        }
+    }
+    // Borrar enemigos
+    for (int i = 0; i < this->enemies.size(); ++i) {
+        bool enemy_removed = false;
+        this->enemies[i]->update();
+
+        for (size_t k = 0; k < this->bullets.size() && !enemy_removed; k++) {
+            if (this->bullets[k]->getBounds().intersects(this->enemies[i]->getBounds())) {
+                this->enemies[i]->setHp(this->enemies[i]->gethp()-this->player->getDamage());
+                if (this->enemies[i]->gethp() <= 0){
+                    this->bullets.erase(this->bullets.cbegin() + k);
+                    this->enemies.erase(this->enemies.cbegin() + i);
+                    this->puntos++;
+                    enemy_removed = true;
+                }
+                else {
+                    this->bullets.erase(this->bullets.cbegin() + k);
+                }
+            }
+        }
+        if(!enemy_removed){
+            // Remover nave cuando llega al otro lado de la ventana (Implementar buzzer de arduino)
+            if (this->enemies[i]->getBounds().left < 0.f)
+            {
+                //Enviar mensaje a Arduino
+                this->enemies.erase(this->enemies.cbegin() + i);
+                char soundAction = 'S';
+                //boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+            }
+        }
+    }
+}
+
+void Medio::initBulletGUI() {
+    if (!this->font.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/fonts/ChakraPetch-Regular.ttf"))
+        std::cout << "ERROR::GAME::Failed to load font" << endl;
+
+    this->bulletsCont.setFont(this->font);
+    this->bulletsCont.setCharacterSize(25);
+    this->bulletsCont.setFillColor(Color::Red);
+    this->bulletsCont.setString("test");
+
+}
+
+void Medio::initBulletCollectorGUI() {
+    if (!this->font.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/fonts/ChakraPetch-Regular.ttf"))
+        std::cout << "ERROR::GAME::Failed to load font" << endl;
+    this->collectorCont.setFont(this->font);
+    this->collectorCont.setCharacterSize(25);
+    this->collectorCont.setFillColor(Color::Red);
+    this->collectorCont.setPosition(600.f, 0.f);
+    this->collectorCont.setString("test");
+
+}
+void Medio::initPointGUI() {
+    if (!this->font.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/fonts/ChakraPetch-Regular.ttf"))
+        std::cout << "ERROR::GAME::Failed to load font" << endl;
+    this->pointsCont.setFont(this->font);
+    this->pointsCont.setCharacterSize(25);
+    this->pointsCont.setFillColor(Color::Red);
+    this->pointsCont.setPosition(670.f, 560.f);
+    this->pointsCont.setString("test");
+
+}
+
+void Medio::initCantEnemies() {
+    if (!this->font.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/fonts/ChakraPetch-Regular.ttf"))
+        std::cout << "ERROR::GAME::Failed to load font" << endl;
+    this->enemiesRest.setFont(this->font);
+    this->enemiesRest.setCharacterSize(25);
+    this->enemiesRest.setFillColor(Color::Red);
+    this->enemiesRest.setPosition(600.f, 30.f);
+    this->enemiesRest.setString("test");
+
+}
+
+void Medio::initOleadasGUI() {
+    if (!this->font.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/fonts/ChakraPetch-Regular.ttf"))
+        std::cout << "ERROR::GAME::Failed to load font" << endl;
+    this->oleadaCont.setFont(this->font);
+    this->oleadaCont.setCharacterSize(25);
+    this->oleadaCont.setFillColor(Color::Red);
+    this->oleadaCont.setPosition(600.f, 60.f);
+    this->oleadaCont.setString("test");
+
+}
+
+void Medio::updateGUI() {
+    stringstream ss, cc, pp, ee, oo;
+    ss << "Balas: " << this->balas;
+    cc << "Collector: " << this->collector;
+    pp << "Puntos: " << this->puntos;
+    ee << "Enemigos: " << this->cant_enemigos;
+    oo << "Oleada: " << this->oleadas;
+    this->bulletsCont.setString(ss.str());
+    this->collectorCont.setString(cc.str());
+    this->pointsCont.setString(pp.str());
+    this->enemiesRest.setString(ee.str());
+    this->oleadaCont.setString(oo.str());
+
+}
+
+void Medio::renderGUI() {
+    this->window->draw(this->bulletsCont);
+    this->window->draw(this->collectorCont);
+    this->window->draw(this->pointsCont);
+    this->window->draw(this->oleadaCont);
+    this->window->draw(this->enemiesRest);
+}
+
+void Medio::initBackground() {
+    if (!this->backgroundTex.loadFromFile("/home/fernandez/datos2/Proyecto1_BattleSpace/Textures/evening_sky.jpg")){
+        cout << "ERROR::FACIL::COULD NOT LOAD BACKGROUND TEXTURE" << endl;
+    }
+    this->background.setTexture(this->backgroundTex);
+    this->background.scale(1.f, 1.f);
+}
+
+void Medio::renderBackground() {
+    this->window->draw(this->background);
+}
+
+void Medio::updateBackground() {
+
+}
+
+void Medio::updateCollision() {
+    // Colision con el borde superior de la pantalla
+    if (this->player->getBounds().top < 0.f){
+        this->player->setPosition(this->player->getBounds().left, 0.f);
+    }
+        // Colision con el borde inferior de la pantalla
+    else if (this->player->getBounds().top + this->player->getBounds().height >= this->window->getSize().y){
+        this->player->setPosition(this->player->getBounds().left, this->window->getSize().y - this->player->getBounds().height);
+    }
+}
+
+void Medio::initSystems() {
+    //this->port.set_option(boost::asio::serial_port_base::baud_rate(9600));
+    this->cant_enemigos = 7;
+    this->oleadas = 1;
+    this->balas = 100;
+    this->collector = 0;
+    this->totalEnemies = 7;
+    this->puntos = 0;
+    this->delay = false;
+    this->canSpawn = true;
+}
+
+void Medio::updateDelay() {
+    if (this->cant_enemigos == 0){
+        this->canSpawn = false;
+        if(enemiesR.empty() && enemies.empty()){
+            this->delay = true;
+            this->delayTimer += 0.5f;
+            if (this->delayTimer >= this->delayTimerMax){
+                this->delay = false;
+                this->canSpawn = true;
+                this->oleadas++;
+
+                /**
+                if (this->oleadas == 2){
+                    char soundAction = '2';
+                    boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+                }
+                if (this->oleadas == 3){
+                    char soundAction = '3';
+                    boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+                }
+                if (this->oleadas == 4){
+                    char soundAction = '4';
+                    boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+                }
+                if (this->oleadas == 5){
+                    char soundAction = '5';
+                    boost::asio::write(port, boost::asio::buffer(&soundAction, 1));
+                }
+                */
+                this->totalEnemies += 3;
+                this->cant_enemigos = this->totalEnemies;
+                cout << "Comienza nueva oleada" << endl;
+                this->delayTimer = 0.f;
+            }
+        }
+    }
+}
+/**
+void Medio::updateArduino() {
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(port, buffer, '\n');
+    std::string message;
+    std::istream input_stream(&buffer);
+    std::getline(input_stream, message);
+    if (message.find("0") != string::npos){
+        this->player->setAttackCooldownMax(60.f);
+    }
+    else if (message.find("1") != string::npos){
+        this->player->setAttackCooldownMax(54.f);
+    }
+    else if (message.find("2") != string::npos){
+        this->player->setAttackCooldownMax(48.f);
+    }
+    else if (message.find("3") != string::npos){
+        this->player->setAttackCooldownMax(42.f);
+    }
+    else if (message.find("4") != string::npos){
+        this->player->setAttackCooldownMax(36.f);
+    }
+    else if (message.find("5") != string::npos){
+        this->player->setAttackCooldownMax(30.f);
+    }
+    else if (message.find("6") != string::npos){
+        this->player->setAttackCooldownMax(24.f);
+    }
+    else if (message.find("7") != string::npos){
+        this->player->setAttackCooldownMax(18.f);
+    }
+    else if (message.find("8") != string::npos){
+        this->player->setAttackCooldownMax(12.f);
+    }
+    else if (message.find("9") != string::npos){
+        this->player->setAttackCooldownMax(6.f);
+    }
+    else if (message.find("U") != string::npos){
+        this->player->move(-1.f);
+    }
+    else if (message.find("D") != string::npos){
+        this->player->move(1.f);
+    }
+
+}
+*/
--- Medio.h
+++ Medio.h
@@ -1,17 +1,85 @@
 #ifndef PROYECTO1_BATTLESPACE_MEDIO_H
 #define PROYECTO1_BATTLESPACE_MEDIO_H
+#include <map>
+#include <string>
+#include <sstream>
+#include <boost/asio.hpp>
 #include "Player.h"
+#include "Bullet.h"
+#include "Enemy.h"
+#include "EnemyR.h"
+#include "Collector.h"
 
 using namespace sf;
+using namespace std;
 
 class Medio {
 private:
+
+    map<string, Texture *> textures;
+    vector<Bullet *> bullets;
+
     RenderWindow* window;
     Player* player;
 
     Font font;
+    Text bulletsCont;
+    Text collectorCont;
+    Text enemiesRest;
+    Text oleadaCont;
+    Text pointsCont;
+
+    //boost::asio::io_service io;
+    //boost::asio::serial_port port = boost::asio::serial_port(io, "/dev/ttyACM0");
+    string input;
+
+    Texture backgroundTex;
+    Sprite background;
+
+    Clock clock; //clock para estrategias
+
+    unsigned balas;
+    unsigned puntos;
+    unsigned cant_enemigos;
+    unsigned oleadas;
+    unsigned collector;
+    unsigned totalEnemies;
+
+    float spawnTimer;
+    float spawnTimerMax;
+
+    float delayTimer;
+    float delayTimerMax;
+
+    float spawnTimerR;
+    float spawnTimerMaxR;
+
+    bool delay;
+    bool canSpawn;
+
+    //para los paged power
+    //bool moveFaster = false;
+    //bool freezeEnemies = false;
+    //bool regenerateBullets = false;
+    //float elapsedTime = 0.f;
+    //bool wkeyPressed = false;
+
+    vector<Enemy*> enemies;
+    vector<EnemyR*> enemiesR;
+
     void initWindow();
+    void initTextures();
+    void initBulletGUI();
+    void initBulletCollectorGUI();
+    void initPointGUI();
+    void initCantEnemies();
+    void initOleadasGUI();
+    void initBackground();
+    void initSystems();
+    void initDelay();
     void initPlayer();
+    void initEnemies();
+    void initEnemiesR();
 
 public:
     Medio();
@@ -19,8 +87,21 @@ public:
 
     void run();
     void updatePollEvents();
+
+
+    //void updateArduino();
+
     void updateInput();
+    void updateGUI();
+    void updateBackground();
+    void updateCollision();
+    void updateBullets();
+    void updateEnemiesAndCombat();
+    void updateEnemiesRAndCombat();
+    void updateDelay();
     void update();
+    void renderGUI();
+    void renderBackground();
     void render();
 
 };
